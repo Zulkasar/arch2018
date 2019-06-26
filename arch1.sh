1a71605ececabe15f0137a95358dc5074181f3a7
Zulkasar <46630124+Zulkasar@users.noreply.github.com>
2019-06-26 23:01:13 +0300

    Update arch1.sh

--- arch1.sh
+++ arch1.sh
@@ -74,4 +74,5 @@ pacstrap /mnt base base-devel
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
+#arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fsSL https://github.com/Zulkasar/arch2018/edit/master/arch2.sh)"
